b6b7dd58787af3b16d54823fefb3531d0667142e
Trivivium <Trivivium@users.noreply.github.com>
2016-10-06 14:08:44 +0200

    Removed toolbar, again!

--- app/src/main/java/com/grp8/weatherapp/MainActivity.java
+++ app/src/main/java/com/grp8/weatherapp/MainActivity.java
@@ -1,37 +1,23 @@
 package com.grp8.weatherapp;
 
 import android.content.Intent;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 
 import com.grp8.weatherapp.controller.Constants;
 
-public class MainActivity extends AppCompatActivity {
-
-
-    Toolbar toolbar;
-
+public class MainActivity extends AppCompatActivity
+{
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
-
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        getSupportActionBar().setTitle("Hejsa");
-        toolbar.setSubtitle("Alle sammen");
-
-//        EditText userID = (EditText) findViewById(R.id.getID);
 
         Intent intent = getIntent();
         Bundle b = intent.getExtras();
 
         String user = b.getString(Constants.KEY_USERID);
-
-//        userID.setText(user);
-
     }
 }
