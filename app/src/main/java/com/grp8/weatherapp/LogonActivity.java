3020ce1e2a95a6abe96eeb9e34db715c93e4b422
Lasse <lassebirkegaard91@gmail.com>
2016-10-06 14:46:47 +0200

    Color-pallete change

--- app/src/main/java/com/grp8/weatherapp/LogonActivity.java
+++ app/src/main/java/com/grp8/weatherapp/LogonActivity.java
@@ -3,6 +3,7 @@ package com.grp8.weatherapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
