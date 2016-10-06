adbe09ef0cc8049fd6e0c19c89f090cda3c76e8a
Lasse <lassebirkegaard91@gmail.com>
2016-10-06 14:32:48 +0200

    Add No title to logon

--- app/src/main/java/com/grp8/weatherapp/LogonActivity.java
+++ app/src/main/java/com/grp8/weatherapp/LogonActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -19,6 +21,9 @@ public class LogonActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logon);
 
