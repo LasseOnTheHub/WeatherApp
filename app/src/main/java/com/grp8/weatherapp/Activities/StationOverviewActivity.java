167c7aca8d1dccc34bd6fd4cb47d83335650a46f
Henrik <Carzita@users.noreply.github.com>
2016-10-20 14:51:06 +0200

    Tilføjet blandende ting, primært i XML filen

--- app/src/main/java/com/grp8/weatherapp/Activities/StationOverviewActivity.java
+++ app/src/main/java/com/grp8/weatherapp/Activities/StationOverviewActivity.java
@@ -1,15 +1,28 @@
 package com.grp8.weatherapp.Activities;
 
+import android.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.grp8.weatherapp.R;
 
 public class StationOverviewActivity extends AppCompatActivity {
 
+    private Button Details;
+    private ImageView WeatherWindow, TempIcon, HumidityIcon, UpdateIcon;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_station_overview);
+
+
+
+
     }
+
+
 }
