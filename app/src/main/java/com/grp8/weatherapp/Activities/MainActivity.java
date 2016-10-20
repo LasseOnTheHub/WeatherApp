ae81d54692cfa4806d6a1c3ec8dd0a97b9fa7f79
Frederik <frederiks96@gmail.com>
2016-10-20 11:40:10 +0200

    Added space below image in stationelement

--- app/src/main/java/com/grp8/weatherapp/Activities/MainActivity.java
+++ app/src/main/java/com/grp8/weatherapp/Activities/MainActivity.java
@@ -3,11 +3,17 @@ package com.grp8.weatherapp.Activities;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.grp8.weatherapp.R;
 
-public class MainActivity extends AppCompatActivity
-{
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -19,6 +25,36 @@ public class MainActivity extends AppCompatActivity
         Bundle b = intent.getExtras();
 
         String user = b.getString(Constants.KEY_USERID);
-        // Frederik test
+
+
+        // RestService.getDevices(user);
+
+        final String[] devices = {
+                "Station 1","Station 2","Station 3","Station 4","Station 5",
+                "Station 6","Station 7", "Station 8","Station 9","Station 10", "Station 11"
+        };
+
+        ArrayAdapter adapter = new ArrayAdapter(this, R.layout.stationlistelement, R.id.station_title,devices) {
+            @Override
+            public View getView(int position, View cachedView, ViewGroup parent) {
+                View view = super.getView(position, cachedView, parent);
+                TextView stationTitle = (TextView) view.findViewById(R.id.station_title);
+                stationTitle.setText(devices[position]);
+
+                return view;
+            }
+        };
+
+        ListView list = (ListView) findViewById(R.id.stationslist);
+        list.setAdapter(adapter);
+        list.setOnItemClickListener(this);
+        list.setSelector(android.R.drawable.ic_notification_overlay);
+
+    }
+
+    public void onItemClick(AdapterView<?> l, View v, int position, long id) {
+        /*Intent intent = new Intent();
+        // RestService.getDevices(user)[position];
+        intent.putExtra(Constants.KEY_SELECTED_STATION, position);*/
     }
 }
