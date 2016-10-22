f0d99c74bd0724a760d123e9d095e9bf8c8a19c6
Frederik <frederiks96@gmail.com>
2016-10-22 14:42:24 +0200

    Station list element

--- app/src/main/java/com/grp8/weatherapp/Activities/MainActivity.java
+++ app/src/main/java/com/grp8/weatherapp/Activities/MainActivity.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
@@ -16,10 +17,11 @@ import android.widget.TextView;
 
 import com.grp8.weatherapp.R;
 
-public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private FrameLayout searchLayout;
     private boolean searchIsVisible = false;
+    private Button mapButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -28,22 +30,25 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
         setContentView(R.layout.activity_main);
 
-        Intent intent = getIntent();
-        Bundle b = intent.getExtras();
-
-        String user = b.getString(Constants.KEY_USERID);
-
-
-        // RestService.getDevices(user);
+        /*
+         * Intent intent = getIntent();
+         * Bundle b = intent.getExtras();
+         * String user = b.getString(Constants.KEY_USERID);
+         * RestService.getDevices(user);
+         */
 
         android.support.v7.app.ActionBar ab = getSupportActionBar();
         ab.setTitle("Choose a station");
 
+        // Dummy data
+        // ###################################################################################
 
         final String[] devices = {
                 "Station 1","Station 2","Station 3","Station 4","Station 5",
                 "Station 6","Station 7", "Station 8","Station 9","Station 10", "Station 11"
         };
+
+        // ###################################################################################
 
         ArrayAdapter adapter = new ArrayAdapter(this, R.layout.stationlistelement, R.id.station_title, devices) {
             @Override
@@ -51,28 +56,27 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
                 View view = super.getView(position, cachedView, parent);
                 TextView stationTitle = (TextView) view.findViewById(R.id.station_title);
                 stationTitle.setText(devices[position]);
+                view.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        System.out.println("Pressed something");
+                    }
+                });
                 return view;
             }
         };
 
         ListView list = (ListView) findViewById(R.id.stationslist);
         list.setAdapter(adapter);
-        list.setOnItemClickListener(this);
-        list.setSelector(android.R.drawable.ic_notification_overlay);
 
         searchLayout = (FrameLayout) findViewById(R.id.searchFrame);
 
         RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(searchLayout.getWidth(), 0);
         searchLayout.setLayoutParams(lp);
-    }
 
-    public void onItemClick(AdapterView<?> l, View v, int position, long id) {
-        /*Intent intent = new Intent();
-        // RestService.getDevices(user)[position];
-        intent.putExtra(Constants.KEY_SELECTED_STATION, position);*/
-        System.out.print("Pressed station " + position+1);
+        mapButton = (Button) findViewById(R.id.mapbutton);
+        mapButton.setOnClickListener(this);
     }
-
 
     // Menu bar
 
@@ -98,9 +102,13 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             case R.id.add_station:
                 break;
         }
-
-
         return true;
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+        System.out.println("Pressed map button");
     }
 
 
