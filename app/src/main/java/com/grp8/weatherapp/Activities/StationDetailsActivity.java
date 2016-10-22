4042d7dd60a67a407865da59cac221e50e18ac46
Trivivium <Trivivium@users.noreply.github.com>
2016-10-22 16:28:12 +0200

    Add sample graph

--- app/src/main/java/com/grp8/weatherapp/Activities/StationDetailsActivity.java
+++ app/src/main/java/com/grp8/weatherapp/Activities/StationDetailsActivity.java
@@ -1,9 +1,13 @@
 package com.grp8.weatherapp.Activities;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.grp8.weatherapp.R;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 
 public class StationDetailsActivity extends AppCompatActivity {
 
@@ -11,5 +15,23 @@ public class StationDetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_station_details);
+
+        ActionBar bar = getSupportActionBar();
+
+        if(bar != null)
+        {
+            bar.setTitle("Station Details");
+        }
+
+        GraphView graph = (GraphView) findViewById(R.id.humidity_graph);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+        graph.addSeries(series);
+
     }
 }
