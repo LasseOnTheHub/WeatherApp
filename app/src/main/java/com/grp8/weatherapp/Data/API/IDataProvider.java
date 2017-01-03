a826a7dda184a2774e3d3e0da5a93a7fc28aca65
Trivivium <Trivivium@users.noreply.github.com>
2017-01-03 16:12:15 +0100

    Added basic API access and tests

---
+++ app/src/main/java/com/grp8/weatherapp/Data/API/IDataProvider.java
@@ -0,0 +1,11 @@
+package com.grp8.weatherapp.Data.API;
+
+import com.grp8.weatherapp.Data.API.Requests.APIRequest;
+
+/**
+ * Created by Thomas on 03-Jan-17.
+ */
+public interface IDataProvider
+{
+    public String fetch(APIRequest request);
+}
