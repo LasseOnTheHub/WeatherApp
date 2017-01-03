977f818849b7761e5c3bb4d7a055c5cd1917d3da
Henrik <Carzita@users.noreply.github.com>
2017-01-03 19:02:45 +0100

    added icons and additional rows with information

---
+++ app/src/main/java/com/grp8/weatherapp/Model/DataReading.java
@@ -0,0 +1,43 @@
+package com.grp8.weatherapp.Model;
+
+/**
+ * Created by Henrik on 03-01-2017.
+ */
+public class DataReading {
+
+    private Float rainFall;
+    private Short relativeAirHumidity;
+    private Short windDirection;
+    private Float windSpeed;
+    private Short lux;
+    private Short airTemp;
+    private Short airPressure;
+
+    public Float getRainFall() {
+        return rainFall;
+    }
+
+    public Short getRelativeAirHumidity() {
+        return relativeAirHumidity;
+    }
+
+    public Short getWindDirection() {
+        return windDirection;
+    }
+
+    public Float getWindSpeed() {
+        return windSpeed;
+    }
+
+    public Short getLux() {
+        return lux;
+    }
+
+    public Short getAirTemp() {
+        return airTemp;
+    }
+
+    public Short getAirPressure() {
+        return airPressure;
+    }
+}
---
+++ app/src/main/java/com/grp8/weatherapp/Model/Station.java
@@ -0,0 +1,20 @@
+package com.grp8.weatherapp.Model;
+
+import com.google.android.gms.maps.model.LatLng;
+
+/**
+ * Created by Henrik on 03-01-2017.
+ */
+public class Station {
+
+    private int id;
+    private LatLng location;
+
+    public int getId() {
+        return id;
+    }
+
+    public LatLng getLocation() {
+        return location;
+    }
+}
