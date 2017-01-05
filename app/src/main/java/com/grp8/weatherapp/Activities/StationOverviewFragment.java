70d616d7546d62f46f5126497d33b969b054ae84
Henrik <Carzita@users.noreply.github.com>
2017-01-05 11:00:26 +0100

    Beginning of converting stationOverview to fragment

---
+++ app/src/main/java/com/grp8/weatherapp/Activities/StationOverviewFragment.java
@@ -0,0 +1,84 @@
+package com.grp8.weatherapp.Activities;
+
+import android.app.ActionBar;
+import android.app.Fragment;
+import android.content.Intent;
+import android.icu.text.DateFormat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TableLayout;
+import android.widget.TextView;
+
+import com.grp8.weatherapp.R;
+import com.grp8.weatherapp.TestData.WeatherStation;
+import com.grp8.weatherapp.TestData.WeatherStations;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class StationOverviewFragment extends Fragment implements View.OnClickListener {
+
+    WeatherStations weatherStations;
+    ArrayList<WeatherStation> weatherStationsArr;
+    private TextView temp,windSpeed,airP, humidity, updated;
+    private TableLayout tableLayout;
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View stationOverview = inflater.inflate(R.layout.activity_station_overview, container, false);
+
+        // setting toolbar as action bar
+        Toolbar toolbar = (Toolbar) stationOverview.findViewById(R.id.toolbar);
+        ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
+
+        // TextView declaration
+        temp = (TextView)stationOverview.findViewById(R.id.temp);
+        windSpeed = (TextView)stationOverview.findViewById(R.id.windSpeed);
+        airP = (TextView)stationOverview.findViewById(R.id.airP);
+        humidity = (TextView)stationOverview.findViewById(R.id.humidity);
+        updated = (TextView)stationOverview.findViewById(R.id.updated);
+
+        // TableLayout declaration
+        tableLayout = (TableLayout)stationOverview.findViewById(R.id.tableLayoutt);
+        tableLayout.setOnClickListener(this);
+
+        weatherStations = WeatherStations.getInstance();
+
+        ArrayList<WeatherStation> stationer = weatherStations.getWeatherStations();
+
+        // android.support.v7.app.ActionBar ab = getSupportActionBar();
+        //  ab.setTitle(stationer.get(1).getTitle());
+        //getSupportActionBar().setTitle(stationer.get(1).getTitle());
+
+        // setting text
+        temp.setText(String.valueOf(stationer.get(1).getWeatherData().getAirTemp()) + " \u2103");
+        windSpeed.setText(String.valueOf(stationer.get(1).getWeatherData().getWindSpeed())+" m/s");
+        airP.setText(String.valueOf(stationer.get(1).getWeatherData().getAirPressure()) + " bar");
+        humidity.setText(String.valueOf(stationer.get(1).getWeatherData().getAirHum())+" %");
+
+        Date date = new Date(stationer.get(1).getWeatherData().getTimeStamp());
+        SimpleDateFormat mdyFormat = new SimpleDateFormat("HH.mm");
+        String mdy = mdyFormat.format(date);
+
+        updated.setText(mdy);
+        return stationOverview;
+    }
+
+    public void onClick(View v) {
+        if(v==tableLayout){
+            Intent i = new Intent(this, StationDetailsActivity.class);
+            startActivity(i);
+        }
+
+    }
+
+}
