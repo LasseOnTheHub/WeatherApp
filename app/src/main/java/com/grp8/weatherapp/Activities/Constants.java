c0a9fa62052c96a284ccb923d292be55e8c76e55
Frederik <frederiks96@gmail.com>
2017-01-05 13:16:58 +0100

    Settings

--- app/src/main/java/com/grp8/weatherapp/Activities/Constants.java
+++ app/src/main/java/com/grp8/weatherapp/Activities/Constants.java
@@ -7,4 +7,5 @@ public class Constants {
     public static final String KEY_USERID = "userid";
     public static final String KEY_WEATHERSTATIONARR = "weatherStationArray";
     public static final String KEY_SELECTED_STATION = "selected_station";
+    public static final String[] SETTINGS = {"Temperature",""};
 }
---
+++ app/src/main/java/com/grp8/weatherapp/Activities/SettingsActivity.java
@@ -0,0 +1,65 @@
+package com.grp8.weatherapp.Activities;
+
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.support.v7.app.ActionBar;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.preference.RingtonePreference;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.grp8.weatherapp.R;
+
+import java.util.List;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        setupActionBar();
+    }
+
+    private void setupActionBar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.myToolbar);
+        setSupportActionBar(toolbar);
+        ActionBar bar = getSupportActionBar();
+        if (bar != null) {
+            bar.setDisplayHomeAsUpEnabled(true);
+            bar.setDisplayShowHomeEnabled(true);
+        }
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+}
