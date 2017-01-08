2a2773dae0c5959841b5064993aa7511b2bbd0ef
Lasse <lassebirkegaard91@gmail.com>
2017-01-09 00:52:51 +0100

    Grafer sat i scrollview.

---
+++ app/src/main/java/com/grp8/weatherapp/Fragments/GraphLuxPressure.java
@@ -0,0 +1,206 @@
+package com.grp8.weatherapp.Fragments;
+
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
+import com.grp8.weatherapp.R;
+import com.grp8.weatherapp.SupportingFiles.Formatters.DayAxisValueFormatter;
+
+import java.util.ArrayList;
+
+/**
+ * Created by lbirk on 09-01-2017.
+ */
+
+public class GraphLuxPressure extends Fragment {
+
+    private LineChart pressureChart;
+    private LineChart luxChart;
+    Typeface mTfLight;
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_lux_pressure, container, false);
+
+        //Opretter grafer
+        pressureChart = (LineChart) view.findViewById(R.id.pressurechart);
+        luxChart = (LineChart) view.findViewById(R.id.luxchart);
+        mTfLight = Typeface.createFromAsset(getActivity().getAssets(), "OpenSans-Light.ttf");
+
+        //******Definere graf for tryk START******
+        pressureChart.setBackgroundColor(Color.rgb(250,250,250));
+        pressureChart.setDrawGridBackground(false);
+        pressureChart.setViewPortOffsets(100f, 100f, 100f, 100f);
+        pressureChart.getDescription().setEnabled(false);
+
+        //Legend
+        Legend pressureLegend = pressureChart.getLegend();
+        pressureLegend.setWordWrapEnabled(true);
+        pressureLegend.setVerticalAlignment(Legend.LegendVerticalAlignment.BOTTOM);
+        pressureLegend.setHorizontalAlignment(Legend.LegendHorizontalAlignment.CENTER);
+        pressureLegend.setOrientation(Legend.LegendOrientation.HORIZONTAL);
+        pressureLegend.setDrawInside(false);
+
+        //Definere tryk's X-akse
+        XAxis PressyreXAxis = pressureChart.getXAxis();
+        PressyreXAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        PressyreXAxis.setAxisMinimum(0f);
+        PressyreXAxis.setGranularity(1f);
+        PressyreXAxis.setValueFormatter(new DayAxisValueFormatter(pressureChart));
+
+        //Definere tryk's Y-akse
+        YAxis PressureYLeftAxis = pressureChart.getAxisLeft();
+        PressureYLeftAxis.setTypeface(mTfLight);
+        PressureYLeftAxis.setTextColor(ColorTemplate.getHoloBlue());
+        PressureYLeftAxis.setAxisMaximum(100);
+        PressureYLeftAxis.setAxisMinimum(0f);
+        PressureYLeftAxis.setDrawGridLines(true);
+        PressureYLeftAxis.setGranularityEnabled(true);
+        //******Definere graf for tryk SLUT******
+
+
+
+        //******Definere graf for lux START******
+        luxChart.getDescription().setEnabled(false);
+        luxChart.setBackgroundColor(Color.rgb(250,250,250));
+        luxChart.setDrawGridBackground(false);
+        luxChart.setViewPortOffsets(100f, 100f, 100f, 100f);
+
+        //Definere lux's legend
+        Legend luxLegend = luxChart.getLegend();
+        luxLegend.setWordWrapEnabled(true);
+        luxLegend.setVerticalAlignment(Legend.LegendVerticalAlignment.BOTTOM);
+        luxLegend.setHorizontalAlignment(Legend.LegendHorizontalAlignment.CENTER);
+        luxLegend.setOrientation(Legend.LegendOrientation.HORIZONTAL);
+        luxLegend.setDrawInside(false);
+
+        //Definere lux's X-akse
+        XAxis luxXAxis = luxChart.getXAxis();
+        luxXAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        luxXAxis.setAxisMinimum(0f);
+        luxXAxis.setGranularity(1f);
+        luxXAxis.setValueFormatter(new DayAxisValueFormatter(luxChart));
+
+
+        //Definere lux's Y-akse
+        YAxis luxLeftAxis = luxChart.getAxisLeft();
+        luxLeftAxis.setTypeface(mTfLight);
+        luxLeftAxis.setTextColor(ColorTemplate.getHoloBlue());
+        luxLeftAxis.setAxisMaximum(100);
+        luxLeftAxis.setAxisMinimum(0f);
+        luxLeftAxis.setDrawGridLines(true);
+        luxLeftAxis.setGranularityEnabled(true);
+        //******Definere graf for lux SLUT******
+
+        setPressyreData();
+        setLuxData();
+        return view;
+    }
+
+    private void setPressyreData() {
+
+        ArrayList<Entry> pressureVals = new ArrayList<Entry>();
+        pressureVals.add(new Entry(1,6));
+        pressureVals.add(new Entry(2,8));
+        pressureVals.add(new Entry(2,9));
+        pressureVals.add(new Entry(3,10));
+        pressureVals.add(new Entry(4,10));
+        pressureVals.add(new Entry(5,5));
+        pressureVals.add(new Entry(6,-0));
+        pressureVals.add(new Entry(7,-5));
+        pressureVals.add(new Entry(8,-5));
+        pressureVals.add(new Entry(9,0));
+
+        LineDataSet set1;
+
+        if (pressureChart.getData() != null &&
+                pressureChart.getData().getDataSetCount() > 0) {
+            set1 = (LineDataSet) pressureChart.getData().getDataSetByIndex(0);
+            set1.setValues(pressureVals);
+            pressureChart.getData().notifyDataChanged();
+            pressureChart.notifyDataSetChanged();
+        } else {
+            // create a dataset and give it a type
+            set1 = new LineDataSet(pressureVals, "Tryk");
+            set1.setMode(LineDataSet.Mode.CUBIC_BEZIER);
+            set1.setCubicIntensity(0.1f);
+            set1.setDrawCircles(true);
+
+            set1.setAxisDependency(YAxis.AxisDependency.LEFT);
+            set1.setColor(ColorTemplate.getHoloBlue());
+            set1.setCircleColor(Color.BLACK);
+            set1.setLineWidth(2f);
+            set1.setCircleRadius(3f);
+            set1.setFillAlpha(100);
+            set1.setFillColor(ColorTemplate.getHoloBlue());
+            set1.setHighLightColor(Color.rgb(244, 117, 117));
+
+            // create a data object with the datasets
+            LineData data = new LineData(set1);
+            data.setValueTextColor(Color.BLACK);
+            data.setValueTextSize(9f);
+
+            // set data
+            pressureChart.setData(data);
+        }
+    }
+    private void setLuxData() {
+
+        ArrayList<Entry> luxVals = new ArrayList<Entry>();
+        luxVals.add(new Entry(1,10));
+        luxVals.add(new Entry(2,20));
+        luxVals.add(new Entry(2,30));
+        luxVals.add(new Entry(3,40));
+        luxVals.add(new Entry(4,0));
+        luxVals.add(new Entry(5,-10));
+        luxVals.add(new Entry(6,20));
+        luxVals.add(new Entry(7,5));
+        luxVals.add(new Entry(8,10));
+        luxVals.add(new Entry(9,20));
+
+        LineDataSet set1;
+
+        if (luxChart.getData() != null &&
+                luxChart.getData().getDataSetCount() > 0) {
+            set1 = (LineDataSet) luxChart.getData().getDataSetByIndex(0);
+            set1.setValues(luxVals);
+            luxChart.getData().notifyDataChanged();
+            luxChart.notifyDataSetChanged();
+        } else {
+            // create a dataset and give it a type
+            set1 = new LineDataSet(luxVals, "Lux");
+            set1.setMode(LineDataSet.Mode.CUBIC_BEZIER);
+            set1.setCubicIntensity(0.1f);
+            set1.setDrawCircles(true);
+
+            //set1.setAxisDependency(YAxis.AxisDependency.LEFT);
+            set1.setColor(ColorTemplate.getHoloBlue());
+            set1.setCircleColor(Color.BLACK);
+            set1.setLineWidth(2f);
+            set1.setCircleRadius(3f);
+            set1.setFillAlpha(100);
+            set1.setFillColor(ColorTemplate.getHoloBlue());
+            set1.setHighLightColor(Color.rgb(244, 117, 117));
+
+            // create a data object with the datasets
+            LineData data = new LineData(set1);
+            data.setValueTextColor(Color.BLACK);
+            data.setValueTextSize(9f);
+
+            // set data
+            luxChart.setData(data);
+        }
+    }
+}
---
+++ app/src/main/java/com/grp8/weatherapp/Fragments/GraphTempRainHumidityFragment.java
@@ -0,0 +1,324 @@
+package com.grp8.weatherapp.Fragments;
+
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.github.mikephil.charting.charts.CombinedChart;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.CombinedData;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.PercentFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
+import com.grp8.weatherapp.R;
+import com.grp8.weatherapp.SupportingFiles.Formatters.DayAxisValueFormatter;
+import com.grp8.weatherapp.SupportingFiles.Formatters.DegreeAxisValueFormatter;
+import com.grp8.weatherapp.SupportingFiles.Formatters.DegreeValueFormatter;
+import com.grp8.weatherapp.SupportingFiles.Formatters.MMAxisValueFormatter;
+import com.grp8.weatherapp.SupportingFiles.Formatters.MMValueFormatter;
+
+import java.util.ArrayList;
+
+/**
+ * Created by lbirk on 08-01-2017.
+ */
+
+
+
+public class GraphTempRainHumidityFragment extends Fragment {
+
+    //Grafer
+    private LineChart humidityChart;
+    private CombinedChart tempRainChart;
+    Typeface mTfLight;
+
+    //Dato vælger
+    private TextView dateInputFrom;
+    private TextView dateInputTo;
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_temp_rain_humidity, container, false);
+        mTfLight = Typeface.createFromAsset(getActivity().getAssets(), "OpenSans-Light.ttf");
+
+        //Grafer
+        humidityChart = (LineChart) view.findViewById(R.id.humiditychart);
+        tempRainChart = (CombinedChart) view.findViewById(R.id.tempandrainchart);
+
+        //Datovælger
+        dateInputFrom = (TextView) view.findViewById(R.id.dateInputFrom);
+        dateInputTo = (TextView) view.findViewById(R.id.dateInputTo);
+
+        //******Definere graf for temperatur og nedbør START******
+
+        tempRainChart.getDescription().setEnabled(false);
+        tempRainChart.setBackgroundColor(Color.rgb(250,250,250));
+        tempRainChart.setDrawGridBackground(false);
+        tempRainChart.setDrawBarShadow(false);
+        tempRainChart.setHighlightFullBarEnabled(false);
+        tempRainChart.setViewPortOffsets(100f, 100f, 100f, 100f);
+
+        //Tegner linjen oven på bars
+        tempRainChart.setDrawOrder(new CombinedChart.DrawOrder[]{
+                CombinedChart.DrawOrder.BAR, CombinedChart.DrawOrder.LINE});
+
+        //Definere temperatur og nedbørs legend
+        Legend tempRainLegend = tempRainChart.getLegend();
+        tempRainLegend.setWordWrapEnabled(true);
+        tempRainLegend.setVerticalAlignment(Legend.LegendVerticalAlignment.BOTTOM);
+        tempRainLegend.setHorizontalAlignment(Legend.LegendHorizontalAlignment.CENTER);
+        tempRainLegend.setOrientation(Legend.LegendOrientation.HORIZONTAL);
+        tempRainLegend.setDrawInside(false);
+
+        //Definere temperatur og nedbørs højre Y-akse
+        YAxis tempRainYRightAxis = tempRainChart.getAxisRight();
+        tempRainYRightAxis.setDrawGridLines(false);
+        tempRainYRightAxis.setAxisMinimum(0f); // this replaces setStartAtZero(true)
+        tempRainYRightAxis.setAxisMaximum(10f);
+        tempRainYRightAxis.setValueFormatter(new MMAxisValueFormatter());
+
+        //Definere temperatur og nedbørs venstre Y-akse
+        YAxis leftAxis = tempRainChart.getAxisLeft();
+        leftAxis.setDrawGridLines(false);
+        leftAxis.setValueFormatter(new DegreeAxisValueFormatter());
+
+        //Definere temperatur og nedbørs X-akse
+        XAxis xAxis = tempRainChart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTH_SIDED);
+        xAxis.setAxisMinimum(0f);
+        xAxis.setGranularity(0f);
+        xAxis.setValueFormatter(new DayAxisValueFormatter(tempRainChart));
+
+        CombinedData data = new CombinedData();
+
+        data.setData(generateTemperatureLineData());
+        data.setData(generateRainBarData());
+        data.setValueTypeface(mTfLight);
+
+        xAxis.setAxisMaximum(data.getXMax() + 0.25f);
+
+        tempRainChart.setData(data);
+
+
+        //******Definere graf for temperatur og nedbør SLUT******
+
+
+        //******Definere graf for fugtighede START******
+
+        humidityChart.getDescription().setEnabled(false);
+        humidityChart.setBackgroundColor(Color.rgb(250,250,250));
+        humidityChart.setDrawGridBackground(false);
+        humidityChart.setViewPortOffsets(100f, 100f, 100f, 100f);
+
+        //Sætter fugtigheds legend
+        Legend humidityLegend = humidityChart.getLegend();
+        humidityLegend.setWordWrapEnabled(true);
+        humidityLegend.setVerticalAlignment(Legend.LegendVerticalAlignment.BOTTOM);
+        humidityLegend.setHorizontalAlignment(Legend.LegendHorizontalAlignment.CENTER);
+        humidityLegend.setOrientation(Legend.LegendOrientation.HORIZONTAL);
+        humidityLegend.setDrawInside(false);
+
+        //Definere fugtigheds X-aksens udseende
+        XAxis humidityXAxis = humidityChart.getXAxis();
+        humidityXAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        humidityXAxis.setAxisMinimum(0f);
+        humidityXAxis.setGranularity(1f);
+        humidityXAxis.setValueFormatter(new DayAxisValueFormatter(humidityChart));
+
+        //Definere fugtigheds venstre Y-akses udseende
+        YAxis HumidityYleftAxis = humidityChart.getAxisLeft();
+        HumidityYleftAxis.setTypeface(mTfLight);
+        HumidityYleftAxis.setTextColor(ColorTemplate.getHoloBlue());
+        HumidityYleftAxis.setAxisMaximum(100);
+        HumidityYleftAxis.setAxisMinimum(0f);
+        HumidityYleftAxis.setDrawGridLines(true);
+        HumidityYleftAxis.setGranularityEnabled(true);
+        HumidityYleftAxis.setValueFormatter(new PercentFormatter());
+
+        //Definere højre Y-akses udseende.
+        YAxis humidityYRightAxis = humidityChart.getAxisRight();
+        humidityYRightAxis.setTypeface(mTfLight);
+        humidityYRightAxis.setTextColor(Color.RED);
+        humidityYRightAxis.setAxisMaximum(100);
+        humidityYRightAxis.setAxisMinimum(0);
+        humidityYRightAxis.setDrawGridLines(false);
+        humidityYRightAxis.setDrawZeroLine(false);
+        humidityYRightAxis.setGranularityEnabled(false);
+        humidityYRightAxis.setValueFormatter(new PercentFormatter());
+        //******Definere graf for fugtighede SLUT******
+
+
+        setHumidityData();
+        return view;
+    }
+
+    private void setHumidityData() {
+
+        //Dummy Data
+        ArrayList<Entry> earthVals = new ArrayList<Entry>();
+        earthVals.add(new Entry(1, 6));
+        earthVals.add(new Entry(2, 8));
+        earthVals.add(new Entry(2, 9));
+        earthVals.add(new Entry(3, 10));
+        earthVals.add(new Entry(4, 10));
+        earthVals.add(new Entry(5, 5));
+        earthVals.add(new Entry(6, -0));
+        earthVals.add(new Entry(7, -5));
+        earthVals.add(new Entry(8, -5));
+        earthVals.add(new Entry(9, 0));
+
+        //Dummy Data
+        ArrayList<Entry> airVals = new ArrayList<Entry>();
+        airVals.add(new Entry(1, 10));
+        airVals.add(new Entry(2, 20));
+        airVals.add(new Entry(2, 15));
+        airVals.add(new Entry(3, 13));
+        airVals.add(new Entry(4, 12));
+        airVals.add(new Entry(5, 5));
+        airVals.add(new Entry(6, 10));
+        airVals.add(new Entry(7, 8));
+        airVals.add(new Entry(8, 7));
+        airVals.add(new Entry(9, 5));
+
+        LineDataSet set1, set2;
+
+        if (humidityChart.getData() != null &&
+                humidityChart.getData().getDataSetCount() > 0) {
+            set1 = (LineDataSet) humidityChart.getData().getDataSetByIndex(0);
+            set2 = (LineDataSet) humidityChart.getData().getDataSetByIndex(1);
+            set1.setValues(earthVals);
+            set2.setValues(airVals);
+            humidityChart.getData().notifyDataChanged();
+            humidityChart.notifyDataSetChanged();
+        } else {
+            // create a dataset and give it a type
+            set1 = new LineDataSet(earthVals, "Jordfugtighed");
+            set1.setMode(LineDataSet.Mode.CUBIC_BEZIER);
+            set1.setCubicIntensity(0.1f);
+            set1.setDrawCircles(true);
+            set1.setAxisDependency(YAxis.AxisDependency.LEFT);
+            set1.setColor(ColorTemplate.getHoloBlue());
+            set1.setCircleColor(Color.BLACK);
+            set1.setLineWidth(2f);
+            set1.setCircleRadius(3f);
+            set1.setFillAlpha(100);
+            set1.setFillColor(ColorTemplate.getHoloBlue());
+            set1.setHighLightColor(Color.rgb(244, 117, 117));
+            set1.setValueFormatter(new PercentFormatter());
+
+            // create a dataset and give it a type
+            set2 = new LineDataSet(airVals, "Luftfugtighed 2");
+            set2.setAxisDependency(YAxis.AxisDependency.RIGHT);
+            set2.setMode(LineDataSet.Mode.CUBIC_BEZIER);
+            set2.setCubicIntensity(0.1f);
+            set2.setColor(Color.RED);
+            set2.setCircleColor(Color.BLACK);
+            set2.setLineWidth(2f);
+            set2.setCircleRadius(3f);
+            set2.setFillAlpha(65);
+            set2.setFillColor(Color.RED);
+            //set2.setDrawCircleHole(false);
+            set2.setHighLightColor(Color.rgb(244, 117, 117));
+            set2.setValueFormatter(new PercentFormatter());
+
+            // create a data object with the datasets
+            LineData data = new LineData(set1, set2);
+            data.setValueTextColor(Color.BLACK);
+            data.setValueTextSize(9f);
+
+            // set data
+            humidityChart.setData(data);
+        }
+    }
+
+    private LineData generateTemperatureLineData() {
+
+        LineData d = new LineData();
+
+        ArrayList<Entry> tempVals = new ArrayList<Entry>();
+
+        tempVals.add(new Entry(1,6));
+        tempVals.add(new Entry(2,8));
+        tempVals.add(new Entry(2,9));
+        tempVals.add(new Entry(3,10));
+        tempVals.add(new Entry(4,10));
+        tempVals.add(new Entry(5,5));
+        tempVals.add(new Entry(6,-0));
+        tempVals.add(new Entry(7,-5));
+        tempVals.add(new Entry(8,-5));
+        tempVals.add(new Entry(9,0));
+
+/*        tempVals.add(new Entry(1451660400,6));
+        tempVals.add(new Entry(1451685600,8));
+        tempVals.add(new Entry(1451721600,10));
+        tempVals.add(new Entry(1451743200,10));
+        tempVals.add(new Entry(1451761200,5));*/
+
+
+        LineDataSet set = new LineDataSet(tempVals, "Temperatur");
+        set.setColor(Color.rgb(242, 72, 0));
+        set.setLineWidth(2.5f);
+        set.setCircleColor(Color.rgb(242, 72, 0));
+        set.setCircleRadius(5f);
+        set.setFillColor(Color.rgb(240, 238, 70));
+        set.setMode(LineDataSet.Mode.CUBIC_BEZIER);
+        set.setCubicIntensity(0.0f);
+        set.setDrawValues(true);
+        set.setValueTextSize(10f);
+        set.setValueTextColor(Color.rgb(242, 72, 0));
+        set.setValueFormatter(new DegreeValueFormatter());
+
+        set.setAxisDependency(YAxis.AxisDependency.LEFT);
+        d.addDataSet(set);
+
+        return d;
+    }
+
+    private BarData generateRainBarData() {
+
+        ArrayList<BarEntry> rainVals = new ArrayList<BarEntry>();
+
+        rainVals.add(new BarEntry(1,1.2f));
+        rainVals.add(new BarEntry(2,1.0f));
+        rainVals.add(new BarEntry(2f,2.0f));
+        rainVals.add(new BarEntry(2f,3.0f));
+        rainVals.add(new BarEntry(3f,0.5f));
+        rainVals.add(new BarEntry(4f,1.0f));
+        rainVals.add(new BarEntry(5f,5.9f));
+        rainVals.add(new BarEntry(6f,2.0f));
+        rainVals.add(new BarEntry(7f,1.7f));
+        rainVals.add(new BarEntry(8f,1.5f));
+        rainVals.add(new BarEntry(9f,1.2f));
+
+
+/*        rainVals.add(new BarEntry(1451660400,1,2));
+        rainVals.add(new BarEntry(1451685600,1,6));
+        rainVals.add(new BarEntry(1451721600,2));
+        rainVals.add(new BarEntry(1451743200,2,5));
+        rainVals.add(new BarEntry(1451761200,3));*/
+
+        BarDataSet set1 = new BarDataSet(rainVals, "Nedbør");
+        set1.setColor(Color.rgb(60, 220, 78));
+        set1.setValueTextColor(Color.rgb(60, 220, 78));
+        set1.setValueTextSize(10f);
+        set1.setAxisDependency(YAxis.AxisDependency.RIGHT);
+        set1.setValueFormatter(new MMValueFormatter());
+
+        float barWidth = 0.90f; //
+        BarData d = new BarData(set1);
+        d.setBarWidth(barWidth);
+        return d;
+    }
+}
