c3c5fe1379b0b3cc5eeb52dedfaddb894eb0a328
Frederik <frederiks96@gmail.com>
2017-01-08 11:54:37 +0100

    SettingsManager

--- app/src/main/java/com/grp8/weatherapp/Model/SettingsManager.java
+++ app/src/main/java/com/grp8/weatherapp/Model/SettingsManager.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import android.preference.PreferenceManager;
 
+import com.grp8.weatherapp.R;
 import com.grp8.weatherapp.SupportingFiles.Constants;
 
 /**
@@ -12,13 +13,64 @@ import com.grp8.weatherapp.SupportingFiles.Constants;
 
 public class SettingsManager {
 
-    Context context = Application.getApplicationContext();
-
-    public static String getTempUnit() {
-        PreferenceManager
-                .getDefaultSharedPreferences(Application.getApplicationContext())
+    public static String getTempUnit(Context context) {
+        return PreferenceManager
+                .getDefaultSharedPreferences(context)
                 .getString(Constants.KEY_TEMP_UNIT, "");
+    }
 
-        return "";
+    public static String getPressureUnit(Context context) {
+        return PreferenceManager
+                .getDefaultSharedPreferences(context)
+                .getString(Constants.KEY_PRESS_UNIT, "");
+    }
+
+    public static String getWindSpeedUnit(Context context) {
+        return PreferenceManager
+                .getDefaultSharedPreferences(context)
+                .getString(Constants.KEY_WS_UNIT, "");
+    }
+
+    public static String getLanguage(Context context) {
+        return PreferenceManager
+                .getDefaultSharedPreferences(context)
+                .getString(Constants.KEY_LANG, "");
+    }
+
+    public static void setupSettings(Context context) {
+        if (getTempUnit(context).equals("")) {
+            String temp = context.getResources().getStringArray(R.array.temp_options_values)[0];
+            PreferenceManager
+                    .getDefaultSharedPreferences(context)
+                    .edit()
+                    .putString(Constants.KEY_TEMP_UNIT, temp)
+                    .apply();
+
+        }
+        if (getPressureUnit(context).equals("")) {
+            String press = context.getResources().getStringArray(R.array.pressure_options)[0];
+            PreferenceManager
+                    .getDefaultSharedPreferences(context)
+                    .edit()
+                    .putString(Constants.KEY_PRESS_UNIT, press)
+                    .apply();
+        }
+        if (getWindSpeedUnit(context).equals("")) {
+            String ws = context.getResources().getStringArray(R.array.windspeed_values)[0];
+            PreferenceManager
+                    .getDefaultSharedPreferences(context)
+                    .edit()
+                    .putString(Constants.KEY_WS_UNIT, ws)
+                    .apply();
+        }
+        if (getLanguage(context).equals("")) {
+            String lang = context.getResources().getStringArray(R.array.language_options)[0];
+            PreferenceManager
+                    .getDefaultSharedPreferences(context)
+                    .edit()
+                    .putString(Constants.KEY_LANG, lang)
+                    .apply();
+        }
+
     }
 }
