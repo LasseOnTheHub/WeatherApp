1fd003693496b1fa22e049a4d22bb4890ba1880d
Trivivium <Trivivium@users.noreply.github.com>
2017-01-09 23:36:02 +0100

    Added global environment file

---
+++ app/src/main/java/com/grp8/weatherapp/SupportingFiles/Environment.java
@@ -0,0 +1,10 @@
+package com.grp8.weatherapp.SupportingFiles;
+
+/*
+ * Created by Thomas on 09-Jan-17.
+ */
+public class Environment
+{
+    public final static int API_MAXIMUM_NUMBER_OF_RETRIES = 5;
+    public final static int API_RETRY_MULTIPLICATION_FACTOR = 3;
+}
