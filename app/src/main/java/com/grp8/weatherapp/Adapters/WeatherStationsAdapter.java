9cd44a7be87b842c6cb9178abdd992f88b92ac0f
Frederik <frederiks96@gmail.com>
2017-01-11 16:41:04 +0100

    Layout and added state

--- app/src/main/java/com/grp8/weatherapp/Adapters/WeatherStationsAdapter.java
+++ app/src/main/java/com/grp8/weatherapp/Adapters/WeatherStationsAdapter.java
@@ -36,6 +36,8 @@ public class WeatherStationsAdapter extends BaseAdapter {
     private Activity activity;
     private ListView list;
 
+    private enum ViewState { Error, Loading, Loaded, LoadedOldContent, LoadedInsaneContent }
+
     public WeatherStationsAdapter(Activity activity, ListView list) {
         super();
         inflater = activity.getLayoutInflater();
@@ -47,7 +49,9 @@ public class WeatherStationsAdapter extends BaseAdapter {
         TextView stationTitle;
         TextView timeLabel;
         TextView tempLabel;
+        LinearLayout timeLayout;
         LinearLayout oldContent;
+        LinearLayout errorLayout;
         ProgressBar tempSpinner;
         ProgressBar timeSpinner;
     }
@@ -57,7 +61,8 @@ public class WeatherStationsAdapter extends BaseAdapter {
 
         final Station station = (Station) getItem(position);
 
-        if (station != null) {
+        if (station != null && convertView == null) {
+            // First load of element and station is not null
             new AsyncTask<Void,DataReading,DataReading>() {
                 @Override
                 protected DataReading doInBackground(Void... arg0) {
@@ -71,9 +76,7 @@ public class WeatherStationsAdapter extends BaseAdapter {
 
                 @Override
                 protected void onPostExecute(DataReading result) {
-                    if (result != null) {
-                        updateListItem(position, result);
-                    }
+                    updateListItem(position, result);
                 }
             }.execute();
         }
@@ -86,7 +89,9 @@ public class WeatherStationsAdapter extends BaseAdapter {
             viewHolder.stationTitle = (TextView) convertView.findViewById(R.id.title_label);
             viewHolder.timeLabel = (TextView) convertView.findViewById(R.id.time_label);
             viewHolder.tempLabel = (TextView) convertView.findViewById(R.id.temp_label);
+            viewHolder.timeLayout = (LinearLayout) convertView.findViewById(R.id.timeLayout);
             viewHolder.oldContent = (LinearLayout) convertView.findViewById(R.id.old_content);
+            viewHolder.errorLayout = (LinearLayout) convertView.findViewById(R.id.error_layout);
             viewHolder.tempSpinner = (ProgressBar) convertView.findViewById(R.id.spinner_temp);
             viewHolder.timeSpinner = (ProgressBar) convertView.findViewById(R.id.spinner_time);
             convertView.setTag(viewHolder);
@@ -105,8 +110,13 @@ public class WeatherStationsAdapter extends BaseAdapter {
 
     private void updateListItem(int index, DataReading reading) {
         View element = list.getChildAt(index);
-        if (element != null) {
-            ViewHolder viewHolder = (ViewHolder) element.getTag();
+
+        if (element == null) {
+            return;
+        }
+
+        ViewHolder viewHolder = (ViewHolder) element.getTag();
+        if (reading != null) {
             viewHolder.tempSpinner.setVisibility(View.GONE);
             viewHolder.timeSpinner.setVisibility(View.GONE);
             viewHolder.timeLabel.setVisibility(View.VISIBLE);
@@ -121,6 +131,11 @@ public class WeatherStationsAdapter extends BaseAdapter {
             } else {
                 viewHolder.oldContent.setVisibility(View.GONE);
             }
+        } else {
+            viewHolder.tempSpinner.setVisibility(View.GONE);
+            viewHolder.timeSpinner.setVisibility(View.GONE);
+            viewHolder.timeLayout.setVisibility(View.GONE);
+            viewHolder.errorLayout.setVisibility(View.VISIBLE);
         }
     }
 
@@ -129,7 +144,7 @@ public class WeatherStationsAdapter extends BaseAdapter {
         cal.setTime(date);
         String time;
         if (cal.get(Calendar.DAY_OF_MONTH) <= 9) {
-            SimpleDateFormat formatter = new SimpleDateFormat("MMM d HH:mm");
+            SimpleDateFormat formatter = new SimpleDateFormat("MMM d HH:mm yyyy");
             time = formatter.format(date);
         } else {
             time = date.toString().substring(4,16);
@@ -137,9 +152,66 @@ public class WeatherStationsAdapter extends BaseAdapter {
         return time;
     }
 
+    private void showState(View element, ViewHolder viewHolder, DataReading reading, ViewState state) {
+        switch (state) {
+            case Error:
+                viewHolder.tempLabel.setVisibility(View.GONE); // Label for temp
+                viewHolder.timeLayout.setVisibility(View.GONE); // Layout for time (including old content)
+                viewHolder.tempSpinner.setVisibility(View.GONE); // Spinner for temp (big one)
+                viewHolder.timeSpinner.setVisibility(View.GONE); // Spinner for time (little one)
+                viewHolder.stationTitle.setVisibility(View.VISIBLE); // Always visible
+                viewHolder.errorLayout.setVisibility(View.VISIBLE); // Label for error
+                // Only error layout and station name is visible
+                return;
+            case Loading:
+                viewHolder.tempLabel.setVisibility(View.GONE); // Label for temp
+                viewHolder.oldContent.setVisibility(View.GONE); // Layout for oldContent
+                viewHolder.timeLabel.setVisibility(View.GONE); // Label for time
+                viewHolder.errorLayout.setVisibility(View.GONE); // Layout for error
+                viewHolder.tempSpinner.setVisibility(View.VISIBLE); // Spinner for temp
+                viewHolder.timeSpinner.setVisibility(View.VISIBLE); // Spinner for time
+                viewHolder.timeLayout.setVisibility(View.VISIBLE); // Layout for time
+                viewHolder.stationTitle.setVisibility(View.VISIBLE); // Always visible
+                // Spinners, clock image and title is visible
+                break;
+            case Loaded:
+                viewHolder.tempLabel.setVisibility(View.VISIBLE); // Label for temp
+                viewHolder.stationTitle.setVisibility(View.VISIBLE); // Always visible
+                viewHolder.timeLabel.setVisibility(View.VISIBLE); // Label for time
+                viewHolder.timeLayout.setVisibility(View.VISIBLE); // Layout for time
+                viewHolder.errorLayout.setVisibility(View.GONE); // Layout for error
+                viewHolder.oldContent.setVisibility(View.GONE); // Layout for old content
+                viewHolder.tempSpinner.setVisibility(View.GONE); // Spinner for temp
+                viewHolder.timeSpinner.setVisibility(View.GONE); // Spinner for time
+                break;
+            case LoadedOldContent:
+                viewHolder.tempLabel.setVisibility(View.VISIBLE); // Label for temp
+                viewHolder.stationTitle.setVisibility(View.VISIBLE); // Always visible
+                viewHolder.timeLabel.setVisibility(View.VISIBLE); // Label for time
+                viewHolder.timeLayout.setVisibility(View.VISIBLE); // Layout for time
+                viewHolder.oldContent.setVisibility(View.VISIBLE); // Layout for old content
+                viewHolder.errorLayout.setVisibility(View.GONE); // Layout for error
+                viewHolder.tempSpinner.setVisibility(View.GONE); // Spinner for temp
+                viewHolder.timeSpinner.setVisibility(View.GONE); // Spinner for time
+                break;
+            case LoadedInsaneContent:
+                viewHolder.tempLabel.setVisibility(View.VISIBLE); // Label for temp
+                viewHolder.stationTitle.setVisibility(View.VISIBLE); // Always visible
+                viewHolder.timeLabel.setVisibility(View.VISIBLE); // Label for time
+                viewHolder.timeLayout.setVisibility(View.VISIBLE); // Layout for time
+                viewHolder.oldContent.setVisibility(View.VISIBLE); // Layout for old content
+                // TODO Set insane content layout
+                viewHolder.errorLayout.setVisibility(View.GONE); // Layout for error
+                viewHolder.tempSpinner.setVisibility(View.GONE); // Spinner for temp
+                viewHolder.timeSpinner.setVisibility(View.GONE); // Spinner for time
+                break;
+        }
+        // TODO Fill labels
+    }
+
     private boolean isOldContent(Date date) {
         Date threshold = new Date(System.currentTimeMillis()-1800000);
-        return date.compareTo(threshold) < 0;
+        return date.before(threshold);
     }
 
     @Override
