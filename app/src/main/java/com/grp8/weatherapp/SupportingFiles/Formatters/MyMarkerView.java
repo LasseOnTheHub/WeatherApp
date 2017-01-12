862b28a8c6e4f6554a80e897308a22a7bfbca75a
Lasse <lassebirkegaard91@gmail.com>
2017-01-12 20:46:10 +0100

    Graf til lux og tryk er linket op mod data. Formattering til at vise dato på x-akse er implementere, samt en markat til at vise præcise datoer på graferne.

---
+++ app/src/main/java/com/grp8/weatherapp/SupportingFiles/Formatters/MyMarkerView.java
@@ -0,0 +1,76 @@
+package com.grp8.weatherapp.SupportingFiles.Formatters;
+import android.content.Context;
+import android.widget.TextView;
+
+import com.github.mikephil.charting.components.MarkerView;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.utils.MPPointF;
+import com.grp8.weatherapp.R;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+
+/**
+ * Created by lbirk on 12-01-2017.
+ */
+
+public class MyMarkerView extends MarkerView {
+
+    private TextView tvContent;
+    private long referenceTimestamp;  // minimum timestamp in your data set
+    private DateFormat mDataFormat;
+    private Date mDate;
+
+    public MyMarkerView (Context context, int layoutResource, long referenceTimestamp) {
+        super(context, layoutResource);
+        // this markerview only displays a textview
+        tvContent = (TextView) findViewById(R.id.tvContent);
+        this.referenceTimestamp = referenceTimestamp;
+        this.mDataFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.ENGLISH);
+        this.mDate = new Date();
+    }
+
+    // callbacks everytime the MarkerView is redrawn, can be used to update the
+    // content (user-interface)
+    @Override
+    public void refreshContent(Entry e, Highlight highlight) {
+        long currentTimestamp = (int)e.getX() + referenceTimestamp;
+
+        tvContent.setText(e.getY() + " hPa : " + getTimedate(currentTimestamp)); // set the entry-value as the display text
+        super.refreshContent(e, highlight);
+    }
+
+/*    @Override
+    public int getXOffset(float xpos) {
+        // this will center the marker-view horizontally
+        return -(getWidth() / 2);
+    }
+
+    @Override
+    public int getYOffset(float ypos) {
+        // this will cause the marker-view to be above the selected value
+        return -getHeight();
+    }*/
+
+
+    @Override
+    public MPPointF getOffset() {
+        return new MPPointF((float)-(getWidth() / 2), (float)-getHeight());
+    }
+
+
+    private String getTimedate(long timestamp){
+
+        try{
+            mDate.setTime(timestamp*1000);
+            return mDataFormat.format(mDate);
+        }
+        catch(Exception ex){
+            return "xx";
+        }
+    }
+}
