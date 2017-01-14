a803d0d9a974f1be07b2a643b4dbae26add8f6c3
Henrik <Carzita@users.noreply.github.com>
2017-01-14 20:39:55 +0100

    indexing

--- app/src/main/java/com/grp8/weatherapp/Fragments/StationOverviewFragment.java
+++ app/src/main/java/com/grp8/weatherapp/Fragments/StationOverviewFragment.java
@@ -9,6 +9,7 @@ import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TableLayout;
 import android.widget.TextView;
+
 import com.grp8.weatherapp.Data.DataRepositoryFactory;
 import com.grp8.weatherapp.Entities.DataReading;
 import com.grp8.weatherapp.Logic.SettingsManager;
@@ -16,11 +17,12 @@ import com.grp8.weatherapp.R;
 import com.grp8.weatherapp.Logic.Constants;
 import com.grp8.weatherapp.Logic.Converters.PressureConverter;
 import com.grp8.weatherapp.Logic.Converters.TemperatureConverter;
+
 import io.fabric.sdk.android.services.concurrency.AsyncTask;
 
 public class StationOverviewFragment extends android.support.v4.app.Fragment implements View.OnClickListener {
 
-    private TextView temp,windSpeed,airP, humidity, updated;
+    private TextView temp, windSpeed, airP, humidity, updated;
     private TableLayout tableLayout;
     private ProgressBar spinner;
     private ImageView weatherWindow;
@@ -29,6 +31,7 @@ public class StationOverviewFragment extends android.support.v4.app.Fragment imp
 
     private class DataRead extends AsyncTask<Void, DataReading, DataReading> {
         final int stationId = getActivity().getIntent().getExtras().getInt(Constants.KEY_STATION_ID);
+
         @Override
         protected DataReading doInBackground(Void... arg0) {
             try {
@@ -38,15 +41,18 @@ public class StationOverviewFragment extends android.support.v4.app.Fragment imp
                 return null;
             }
         }
+
         @Override
         protected void onPostExecute(DataReading reading) {
-               if(reading != null){
-                   updateView(reading);
-               } else errorView();
+            if (reading != null) {
+                updateView(reading);
+            } else errorView();
         }
-    };
+    }
 
-    public class TaskCanceler implements Runnable{
+    ;
+
+    public class TaskCanceler implements Runnable {
         private AsyncTask task;
 
         public TaskCanceler(AsyncTask task) {
@@ -55,37 +61,35 @@ public class StationOverviewFragment extends android.support.v4.app.Fragment imp
 
         @Override
         public void run() {
-            if (task.getStatus() == AsyncTask.Status.RUNNING )
+            if (task.getStatus() == AsyncTask.Status.RUNNING)
                 task.cancel(true);
-                errorView();
+            errorView();
         }
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View stationOverview = inflater.inflate(R.layout.fragment_station_overview, container, false);
 
         // TextView declaration
-        temp = (TextView)stationOverview.findViewById(R.id.temp);
-        windSpeed = (TextView)stationOverview.findViewById(R.id.windSpeed);
-        airP = (TextView)stationOverview.findViewById(R.id.airP);
-        humidity = (TextView)stationOverview.findViewById(R.id.humidity);
-        updated = (TextView)stationOverview.findViewById(R.id.updated);
-        spinner = (ProgressBar)stationOverview.findViewById(R.id.spinner);
+        temp = (TextView) stationOverview.findViewById(R.id.temp);
+        windSpeed = (TextView) stationOverview.findViewById(R.id.windSpeed);
+        airP = (TextView) stationOverview.findViewById(R.id.airP);
+        humidity = (TextView) stationOverview.findViewById(R.id.humidity);
+        updated = (TextView) stationOverview.findViewById(R.id.updated);
+        spinner = (ProgressBar) stationOverview.findViewById(R.id.spinner);
 
         // initiates asynctask and cancels it after 15 sec if it hasn't finished
         loadData();
 
         // ImageView declaration
-        weatherWindow = (ImageView)stationOverview.findViewById(R.id.weatherWindow);
+        weatherWindow = (ImageView) stationOverview.findViewById(R.id.weatherWindow);
 
         // setting spinner visible and loading text
         loadedView();
 
         // TableLayout declaration
-        tableLayout = (TableLayout)stationOverview.findViewById(R.id.tableLayoutt);
-
+        tableLayout = (TableLayout) stationOverview.findViewById(R.id.tableLayoutt);
 
         return stationOverview;
     }
@@ -102,7 +106,7 @@ public class StationOverviewFragment extends android.support.v4.app.Fragment imp
         // setting text
 
         // set temperature text and getting the appropriate unit
-        String tem = String.valueOf(TemperatureConverter.getFormattedTemp(getActivity().getApplicationContext(),reading.getAirReadings().getTemperature()));
+        String tem = String.valueOf(TemperatureConverter.getFormattedTemp(getActivity().getApplicationContext(), reading.getAirReadings().getTemperature()));
         temp.setText(tem + " " + SettingsManager.getTempUnit(getActivity().getApplicationContext()));
 
         // set windspeed text and getting the appropriate unit
@@ -111,13 +115,12 @@ public class StationOverviewFragment extends android.support.v4.app.Fragment imp
         windSpeed.setText(speed + " " + SettingsManager.getWindSpeedUnit(getActivity().getApplicationContext()));
 
         // set pressure text and getting the appropriate unit
-        String pressure = String.valueOf(PressureConverter.getFormattedPressure(getActivity().getApplicationContext(),reading.getAirReadings().getPressure()));
+        String pressure = String.valueOf(PressureConverter.getFormattedPressure(getActivity().getApplicationContext(), reading.getAirReadings().getPressure()));
         airP.setMaxWidth(315);
         airP.setText(pressure + " " + SettingsManager.getPressureUnit(getActivity().getApplicationContext()));
-        if(reading.getAirReadings().getPressure()<1000){
+        if (reading.getAirReadings().getPressure() < 1000) {
             weatherWindow.setImageResource(R.mipmap.cloudy);
-        }
-        else {
+        } else {
             weatherWindow.setImageResource(R.mipmap.sunny);
         }
 
@@ -129,6 +132,7 @@ public class StationOverviewFragment extends android.support.v4.app.Fragment imp
         updated.setMaxWidth(315);
         updated.setText(String.valueOf(reading.getTimestamp()));
     }
+
     private void loadedView() {
         spinner.setVisibility(View.VISIBLE);
         temp.setText(R.string.loadingTextOverview);
@@ -147,12 +151,13 @@ public class StationOverviewFragment extends android.support.v4.app.Fragment imp
         updated.setText(R.string.error);
 
     }
-    private void loadData(){
+
+    private void loadData() {
         DataRead task = new DataRead();
         asyncCanceler = new TaskCanceler(task);
-        handler.postDelayed(asyncCanceler, 15*1000);
+        handler.postDelayed(asyncCanceler, 15 * 1000);
         task.execute();
-        if(asyncCanceler != null && handler != null) {
+        if (asyncCanceler != null && handler != null) {
             handler.removeCallbacks(asyncCanceler);
         }
     }
