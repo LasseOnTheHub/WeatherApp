ed913c7caafd54c1ee59085cac54c46f898ad54c
Trivivium <Trivivium@users.noreply.github.com>
2017-01-15 19:31:54 +0100

    Add language strings and made a general code cleanup

--- app/src/main/java/com/grp8/weatherapp/Activities/LogonActivity.java
+++ app/src/main/java/com/grp8/weatherapp/Activities/LogonActivity.java
@@ -22,64 +22,59 @@ import io.fabric.sdk.android.Fabric;
 
 public class LogonActivity extends AppCompatActivity {
 
-    private EditText passwordEditText;
     private EditText userIDEditText;
-    private TextInputLayout inputLayoutName, inputLayoutPassword;
-
-    Authorizer authroizer;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        //this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        //this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
 
         if (!Utils.isEmulator()) {
             Fabric.with(this, new Crashlytics());
         }
+
         setContentView(R.layout.activity_logon);
 
-        inputLayoutName = (TextInputLayout) findViewById(R.id.userIDTextLayout);
-        inputLayoutPassword = (TextInputLayout) findViewById(R.id.userPasswordTextLayout);
-        passwordEditText = (EditText) findViewById(R.id.userPasswordText);
-        userIDEditText = (EditText) findViewById(R.id.userIDText);
-        authroizer = new Authorizer();
+        this.userIDEditText = (EditText) findViewById(R.id.userIDText);
     }
 
-    public void login(View view) {
+    public void login(View view)
+    {
         if (userIDEditText.getText().toString().matches(""))
         {
-            Toast.makeText(this, "Indtast brugernavn", Toast.LENGTH_SHORT).show(); // TODO: add lang string
+            Toast.makeText(this, R.string.logon_toast_username, Toast.LENGTH_SHORT).show();
         }
         else
         {
-            String userId = userIDEditText.getText().toString();
-            int userIdInt = Integer.parseInt(userId);
-            if (DataRepositoryFactory.build(getApplicationContext()).authorize(userIdInt)) {
-                Toast.makeText(this, "Success", Toast.LENGTH_SHORT).show();
-                if (!Utils.isEmulator()) {
-                    logUser(userId);
+            int userID = Integer.parseInt(userIDEditText.getText().toString());
+
+            if (DataRepositoryFactory.build(getApplicationContext()).authorize(userID))
+            {
+                Toast.makeText(this, R.string.logon_success, Toast.LENGTH_SHORT).show();
+
+                if (!Utils.isEmulator())
+                {
+                    logUser(Integer.toString(userID));
                 }
 
                 DataRepositoryFactory.build(getApplicationContext()).setUser(5);
+                SettingsManager.setupSettings(getApplicationContext());
 
                 Intent intent = new Intent(LogonActivity.this,MainActivityTab.class);
-                intent.putExtra(Constants.KEY_USERID,userId);
+                intent.putExtra(Constants.KEY_USERID, userID);
+
                 startActivity(intent);
-
-                SettingsManager.setupSettings(getApplicationContext());
                 finish();
-
-
-            } else {
-                Toast.makeText(this, R.string.forkert_login, Toast.LENGTH_SHORT).show();
             }
-
+            else
+            {
+                Toast.makeText(this, R.string.logon_failure, Toast.LENGTH_SHORT).show();
+            }
         }
     }
-    //Bruges til at logge brugeren til identifikation under nedbrudsrapportering
-    private void logUser(String userName) {
+
+    private void logUser(String userName)
+    {
         Crashlytics.setUserIdentifier(userName);
         Crashlytics.setUserEmail("Ikke angivet");
         Crashlytics.setUserName("Ikke angivet");
