63822e67f09c87d3a635f62ed2f675bbbba6ecd3
Trivivium <Trivivium@users.noreply.github.com>
2017-01-15 19:40:15 +0100

    Added station name in toolbar and language strings

--- app/src/main/java/com/grp8/weatherapp/Activities/WeatherStationTab.java
+++ app/src/main/java/com/grp8/weatherapp/Activities/WeatherStationTab.java
@@ -1,19 +1,22 @@
 package com.grp8.weatherapp.Activities;
 
-import android.support.design.widget.TabLayout;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBar;
+import android.support.v4.view.ViewPager;
 import android.support.v7.widget.Toolbar;
+import android.support.design.widget.TabLayout;
 import android.view.MenuItem;
+import android.os.Bundle;
 
-import com.grp8.weatherapp.Fragments.DatePickerFragment;
-import com.grp8.weatherapp.Fragments.GraphLuxPressure;
+import com.grp8.weatherapp.Data.DataRepositoryFactory;
+import com.grp8.weatherapp.Entities.Station;
 import com.grp8.weatherapp.Fragments.GraphTempRainHumidityFragment;
 import com.grp8.weatherapp.Fragments.StationOverviewFragment;
+import com.grp8.weatherapp.Fragments.DatePickerFragment;
+import com.grp8.weatherapp.Fragments.GraphLuxPressure;
 import com.grp8.weatherapp.Logic.Constants;
 import com.grp8.weatherapp.R;
 
@@ -21,37 +24,56 @@ import java.util.Date;
 
 public class WeatherStationTab extends AppCompatActivity
 {
-
     private Date startDate = new Date(System.currentTimeMillis()-604800000);
     private Date endDate = new Date();
-    public int stationId;
+
+    private int stationID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main_tab);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        this.stationID = getIntent().getExtras().getInt(Constants.KEY_STATION_ID);
 
-        SectionsPagerAdapter mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Station station = DataRepositoryFactory.build(getApplicationContext()).getStation(this.stationID);
+
+        if(station != null)
+        {
+            toolbar.setTitle(station.getNotes());
+        }
+
+        setSupportActionBar(toolbar);
+
+        ActionBar supportActionBar = getSupportActionBar();
+
+        if(supportActionBar != null)
+        {
+            supportActionBar.setDisplayHomeAsUpEnabled(true);
+            supportActionBar.setDisplayShowHomeEnabled(true);
+        }
 
         ViewPager mViewPager = (ViewPager) findViewById(R.id.container);
-        mViewPager.setAdapter(mSectionsPagerAdapter);
-        stationId = getIntent().getExtras().getInt(Constants.KEY_STATION_ID);
+        mViewPager.setAdapter(new SectionsPagerAdapter(getSupportFragmentManager()));
+
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
         tabLayout.setTabMode(TabLayout.MODE_FIXED);
         tabLayout.setupWithViewPager(mViewPager);
     }
 
+    public int getCurrentStationID()
+    {
+        return this.stationID;
+    }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item)
     {
-        if (item.getItemId() == android.R.id.home)
+        if(item.getItemId() == android.R.id.home)
         {
             finish();
         }
@@ -59,30 +81,34 @@ public class WeatherStationTab extends AppCompatActivity
         return true;
     }
 
-    public void setFromDate(Date date) {
-        startDate = date;
+    public void setFromDate(Date date)
+    {
+        this.startDate = date;
+
         ((DatePickerFragment)getSupportFragmentManager().getFragments().get(1)).setFromDate(date);
         ((DatePickerFragment)getSupportFragmentManager().getFragments().get(2)).setFromDate(date);
     }
 
-    public void setToDate(Date date) {
-        endDate = date;
+    public void setToDate(Date date)
+    {
+        this.endDate = date;
 
         ((DatePickerFragment)getSupportFragmentManager().getFragments().get(1)).setToDate(date);
         ((DatePickerFragment)getSupportFragmentManager().getFragments().get(2)).setToDate(date);
     }
 
-    public Date getStartDate() {
-        return startDate;
+    public Date getStartDate()
+    {
+        return this.startDate;
     }
 
-    public Date getEndDate() {
-        return endDate;
+    public Date getEndDate()
+    {
+        return this.endDate;
     }
 
     public class SectionsPagerAdapter extends FragmentPagerAdapter
     {
-
         public SectionsPagerAdapter(FragmentManager fm)
         {
             super(fm);
@@ -91,11 +117,16 @@ public class WeatherStationTab extends AppCompatActivity
         @Override
         public Fragment getItem(int position)
         {
-            if (position == 0) {
+            if(position == 0)
+            {
                 return new StationOverviewFragment();
-            } else if (position == 1) {
+            }
+            else if(position == 1)
+            {
                 return new GraphTempRainHumidityFragment();
-            } else {
+            }
+            else
+            {
                 return new GraphLuxPressure();
             }
         }
@@ -109,17 +140,22 @@ public class WeatherStationTab extends AppCompatActivity
         @Override
         public CharSequence getPageTitle(int position)
         {
-            switch (position)
+            int resource = R.string.station_tabs_unknown;
+
+            switch(position)
             {
                 case 0 :
-                    return "Overview";
+                    resource = R.string.station_tabs_overview;
+                    break;
                 case 1 :
-                    return "Temp+Rain";
+                    resource = R.string.station_tabs_temperature_rain;
+                    break;
                 case 2 :
-                    return "Pressure+Lux";
-                default:
-                    return "";
+                    resource = R.string.station_tabs_pressure_lux;
+                    break;
             }
+
+            return getString(resource);
         }
     }
 }
